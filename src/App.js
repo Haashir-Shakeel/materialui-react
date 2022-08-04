af3b2751560d91c640d338f6fa77beea87919a1c
HaashirBinShakeel <56473466+HaashirBinShakeel@users.noreply.github.com>
2022-08-04 17:40:32 +0500

    buttons, theme, custom button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
-import {Button} from '@mui/material'
+import {Button, styled, Typography} from '@mui/material'
+
+import {Add, Delete, Settings} from '@mui/icons-material'
+
+
 
 function App() {
+  const UniqueButton = styled(Button)(({theme})=>({
+    backgroundColor: theme.palette.otherColor,
+        color:"#888",
+        "&:hover":{
+          backgroundColor: "red",
+        },
+        "&:disabled":{
+          backgroundColor: "gray",
+          color: "red",
+        }
+  }))
+
   return (
     <div>
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
+      <Button variant="contained" endIcon={<Add/>} color="success">Add New Post</Button>
+      <Button variant="contained" color='otherColor' startIcon={<Settings/>}>Settings</Button>
+      <Button variant="outlined" endIcon={<Delete/>}>Delete</Button>
+
+      <Typography variant="h3" component="div" gutterBottom>
+        h1. Heading
+      </Typography>
+
+      <Button variant='contained' disabled sx={{
+        backgroundColor: "darkgreen",
+        color:"#888",
+        "&:hover":{
+          backgroundColor: "red",
+        },
+        "&:disabled":{
+          backgroundColor: "gray",
+          color: "red"
+        }
+      }} >Unique Button</Button>
+            
+            <UniqueButton>My Button</UniqueButton>
+
+
     </div>
   );
 }
