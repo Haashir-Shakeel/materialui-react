eabbb3b75662a782f03728926f97c465464f5448
HaashirBinShakeel <56473466+HaashirBinShakeel@users.noreply.github.com>
2022-08-09 01:52:57 +0500

    search box, InputBase, Icons, Badge, main design responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,21 @@
-import { AppBar, styled, Toolbar, Typography } from "@mui/material"
-import {Hive} from '@mui/icons-material'
+import { InputBase,AppBar, Box, styled, Toolbar, Typography, Badge } from "@mui/material"
+import {Hive, Mail, Notifications} from '@mui/icons-material'
+
 const StyledToolbar = styled(Toolbar)({
     display: "flex",
     justifyContent: "space-between"
 }) 
+
+const Search = styled('div')(({theme})=>({
+    backgroundColor: "white",
+    padding:"0px 10px",
+    borderRadius:theme.shape.borderRadius,
+    width:"40%"
+}))
+
+const Icons = styled(Box)(({theme})=>({
+
+}))
 
 export const Navbar = ()=>{
     return(
@@ -12,7 +24,17 @@ export const Navbar = ()=>{
                 <Typography variant="h6" sx={{display:{xs:"none",sm:"block"}}}>
                     MIRANDA
                 </Typography>
-                <Hive/>
+                {/* //an icon here */}
+                <Hive sx={{display:{xs:"block",sm:"none"}}}/>
+                <Search><InputBase placeholder="search..."/> </Search> 
+                <Icons>
+                    <Badge badgeContent={4} color="error">
+                        <Mail/>
+                    </Badge>
+                    <Badge badgeContent={2} color="error">
+                        <Notifications/>
+                    </Badge>
+                </Icons>
             </StyledToolbar>
         </AppBar>
     )
