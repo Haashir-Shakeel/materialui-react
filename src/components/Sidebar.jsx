d0bcf3649555abb9a5b69d073832ec9630319612
HaashirBinShakeel <56473466+HaashirBinShakeel@users.noreply.github.com>
2022-08-09 15:49:28 +0500

    sidebar List and ListItems

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,76 @@
-import { Box } from "@mui/material"
-
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material"
+import {AccountBox, Article, Group, Home, Person, Settings, Storefront} from '@mui/icons-material'
 export const Sidebar = ()=>{
     return(
-        <Box 
-        bgcolor="skyblue" 
+        <Box  
         flex={1} p={2}
         sx={{display: {xs:"none",sm:"block"}}}>
-            SideBar
+            
+        <List>
+            <ListItem disablePadding>
+                <ListItemButton component="a" href="#home">
+                    <ListItemIcon>
+                        <Home/>
+                    </ListItemIcon>
+                    <ListItemText primary="Homepage"/>
+                </ListItemButton>
+            </ListItem>
+
+            <ListItem disablePadding>
+                <ListItemButton component="a" href="#simple-list">
+                    <ListItemIcon>
+                        <Article/>
+                    </ListItemIcon>
+                    <ListItemText primary="Pages"/>
+                </ListItemButton>
+            </ListItem>
+
+            <ListItem disablePadding>
+                <ListItemButton component="a" href="#simple-list">
+                    <ListItemIcon>
+                        <Group/>
+                    </ListItemIcon>
+                    <ListItemText primary="Groups"/>
+                </ListItemButton>
+            </ListItem>
+
+            <ListItem disablePadding>
+                <ListItemButton component="a" href="#simple-list">
+                    <ListItemIcon>
+                        <Storefront/>
+                    </ListItemIcon>
+                    <ListItemText primary="Marketplace"/>
+                </ListItemButton>
+            </ListItem>
+
+            <ListItem disablePadding>
+                <ListItemButton component="a" href="#simple-list">
+                    <ListItemIcon>
+                        <Person/>
+                    </ListItemIcon>
+                    <ListItemText primary="Friends"/>
+                </ListItemButton>
+            </ListItem>
+
+            <ListItem disablePadding>
+                <ListItemButton component="a" href="#simple-list">
+                    <ListItemIcon>
+                        <Settings/>
+                    </ListItemIcon>
+                    <ListItemText primary="Settings"/>
+                </ListItemButton>
+            </ListItem>
+
+            <ListItem disablePadding>
+                <ListItemButton component="a" href="#simple-list">
+                    <ListItemIcon>
+                        <AccountBox/>
+                    </ListItemIcon>
+                    <ListItemText primary="Profile"/>
+                </ListItemButton>
+            </ListItem>
+        </List>
+
         </Box>
     )
 }
